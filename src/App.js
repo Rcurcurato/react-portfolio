41629c5d98f94669571719f5b1f900cce34cffb4
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-10-22 19:41:33 -0400

    imported components into app js. Added index.js to my component folders.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+
+function App() {
+    
+}
+
+export default App;
