c34551db151d46db13232a526bc1bd9959a7ed96
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-10-22 19:50:27 -0400

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
+import About from '..src/components/About';
+import Contact from '..src/components/Contact';
+import Footer from '..src/components/Footer';
+import Header from '..src/components/Header';
+import Portfolio from '..src/components/Portfolio';
+import {helmet} from 'react-helmet';
+import Resume from '..src/components/Resume';
 
 function App() {
     
