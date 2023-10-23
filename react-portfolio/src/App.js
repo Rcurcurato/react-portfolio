d265570a837fe04bbbd54136794b9469051b6728
Rcurcurato <133296185+Rcurcurato@users.noreply.github.com>
2023-10-22 21:18:34 -0400

    imported react to component files:

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,3 +1,11 @@
+import React, { useState } from 'react';
+import About from '..src/components/about';
+import Contact from '..src/components/contact';
+import Footer from '..src/components/footer';
+import Header from '..src/components/header';
+import Portfolio from '..src/components/portfolio';
+import Resume from '..src/components/resume';
+import { Helmet } from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
